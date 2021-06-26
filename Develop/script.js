273b718b6f8f208bf502b7a195734f3e4dd234da
Kait <kweishaar@hawk.iit.edu>
2021-06-26 10:54:19 -0500

    persistence,sucess

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,14 @@ let button = $(".saveBtn")
     let setDate = moment().format("dddd MMMM Do YYYY");
     $("#currentDay").text(setDate);
     colorCode();
+    //Add persistence to events
+button.on('click', function (event) {
+    event.preventDefault();
+    var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
+    var parent = $(this).parent().attr('id');
+    localStorage.setItem(parent, JSON.stringify(text));
+
+  })
 })
 
 
@@ -27,31 +35,14 @@ function colorCode() {
     })
 }
 
-//Add persistence to events
-button.click = function (event) {
-    //stop automatic reload from wiping page
-    event.preventDefault();
 
 
-    //Get info from task- ie user input and which hour it is in (parent)
-    let task = $(this).siblings(".col-sm-10").val().replace(/['"]+/g, '');
-    let parent = $(this).parent().attr('id');
-
-    //Store tasks in local storage
-    localStorage.setItem(parent, JSON.stringify(task));
-
-}
-
-
-//Get tasks from local storage and replace text area val with items from local storage
-
-
-$('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
-$('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
-$('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
-$('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
-$('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
-$('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
-$('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
-$('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
-$('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
+  $('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
+  if (text) {$('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''))};
+  $('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
+  $('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
+  $('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
+  $('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
+  $('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
+  $('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
+  $('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
