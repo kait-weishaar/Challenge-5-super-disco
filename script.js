68f8edf65f8356ae25e546cda428d9e6c40fe13c
Kait <kweishaar@hawk.iit.edu>
2021-06-27 18:07:45 -0500

    better storage

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+$(document).ready(function() {
 //Link to save button
 let button = $(".saveBtn");
 
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem('eventArray')) || ["", "", "", "", "", "", "", "", ""] // 9, 10, 11, 12  0+9=>index and know the hour
+  }
+  
+//   setLocalStorage("words", 17)
+  function setLocalStorage(eventToAdd, hourToAddTo) {
+      // its important what time we put it in as
+    var currentLS = getLocalStorage(); // ["", *9] || ["" *9] 
+    currentLS[hourToAddTo-9] = eventToAdd; // =>currentLS[8] => ["", "", "", "", "", "", "", "words", ""]
+    localStorage.setItem('eventArray', JSON.stringify(currentLS));
+    return;
+  }
 //Link to date holder at top of page
-$(document).ready(function() {
     let setDate = moment().format("dddd MMMM Do YYYY");
     $("#currentDay").text(setDate);
     colorCode();
@@ -11,38 +24,38 @@ button.on('click', function (event) {
     event.preventDefault();
     var text = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
     var parent = $(this).parent().attr('id');
-    localStorage.setItem(parent, JSON.stringify(text));
-
+    
+    setLocalStorage(text, parent);
   })
+  
+  let text = null;
+  //Style time blocks based on relationship to present time
+  function colorCode() {
+      let now = moment().hours();
+      let block = $(".time-block");
+      
+      block.each(function () {
+          let hour = parseInt($(this).attr("id"));
+          
+          if(hour === now) {
+              $(this).children(".col-sm-10").addClass("present");
+            } else if (now > hour){
+                $(this).children(".col-sm-10").addClass("past");
+            } else {
+                $(this).children(".col-sm-10").addClass("future");
+            }
+        })
+    }
+    
+    
+    function fillEvents() {
+        let currentLS = getLocalStorage();
+        
+        //Testing $('#9 textarea').val(currentLS[0]);
+        currentLS.forEach((singularTime, i) => {
+            $(`#${i+9} textarea`).val(singularTime)
+        })
+        
+    }
+    fillEvents();
 })
-
-let text = null;
-//Style time blocks based on relationship to present time
-function colorCode() {
-    let now = moment().hours();
-    let block = $(".time-block");
-
-    block.each(function () {
-        let hour = parseInt($(this).attr("id"));
-
-        if(hour === now) {
-            $(this).children(".col-sm-10").addClass("present");
-        } else if (now > hour){
-            $(this).children(".col-sm-10").addClass("past");
-        } else {
-            $(this).children(".col-sm-10").addClass("future");
-        }
-    })
-}
-
-
-
-  $('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
-  $('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
-  $('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
-  $('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
-  $('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
-  $('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
-  $('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
-  $('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
-  $('#17 textarea').val(localStorage.getItem('17')).replace(/['"]+/g, '');
